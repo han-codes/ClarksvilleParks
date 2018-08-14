2c3240299d67e45e57d6e78a05c52aad61801df1
Hannie Kim <hkim15@adminisatorsmbp.apsu.edu>
2018-08-14 08:42:53 -0500

    Initial Commit

---
+++ ClarksvilleParks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClarksvilleParks
+//
+//  Created by Hannie Kim on 8/14/18.
+//  Copyright © 2018 Hannie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
